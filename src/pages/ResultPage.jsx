c47e566f0e6f0733c20973d34f9fda72630c1e0f
sri nanveenrajr <srinaveenraj6@gmail.com>
2026-01-20 15:02:44 +0530

    9th  commit

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 
 export default function ResultPage() {
   const { id } = useParams();
